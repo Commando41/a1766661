4a8ef35e5828f94dba95b7d859beccb149dfed51
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-06 02:31:32 +0000

    Update

--- 2019/s2/oop/workshop-02/transform.cpp
+++ 2019/s2/oop/workshop-02/transform.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
+int main(){
+
+std::string output;
+int number;
+
+std::cout << "Enter input" << std::endl;
+std::cin >> number;
+
+while (number > 0){
+if (number % 2 == 1){
+output = "1" + output;
+number = round(number / 2) - 1;
+}else{
+output = "0" + output;
+number = number / 2;
+}
+
+}
+
+std::cout << output << std::endl;
+
+return 0;
+}
 
