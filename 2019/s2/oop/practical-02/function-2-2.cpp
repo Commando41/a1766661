1953d0fe833b721f1df5ba51b141360099af4c40
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-09 14:16:59 +0000

    Update 2-3

--- 2019/s2/oop/practical-02/function-2-2.cpp
+++ 2019/s2/oop/practical-02/function-2-2.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+int pow(int base, int index)
+{
+int power;
+power = base^index;
+return power;
+}
+
 int binary_to_number(int binary_digits[], int number_of_digits){
 
 int value = 0;
 
-for (int i = 0 ; i < number_of_digits; i++){
+for (int i = 0; i < number_of_digits; i++){
 if (binary_digits[i] == 1){
-value = value + pow(2, number_of_digits - 1 - i);
+value = value + (pow(2, number_of_digits - 1 - i));
 std::cout << value << std::endl;
 }
 }
--- 2019/s2/oop/practical-02/function-2-3.cpp
+++ 2019/s2/oop/practical-02/function-2-3.cpp
@@ -1,6 +1,9 @@
-#include <math.h>
-bool sum_if_a_palinedrome(int integers[], int length)
+
+bool is_a_palinedrome(int integers[], int length)
 {
+if (length < 0){
+return false;
+}
 for (int i = 0; i < length/2; i++){
 if (integers[i] != integers[length - i - 1]){
 return false;
@@ -9,9 +12,11 @@ return false;
 return true;
 }
 
-int is_a_palinedrome(int integers[], int length)
+int sum_elements(int integers[], int length)
 {
-
+if (length < 0){
+return -2;
+}
 int sum = 0;
 
 for (int i = 0; i < length; i++){
@@ -21,12 +26,14 @@ sum = sum + integers[i];
 return sum;
 }
 
-int sum_elements(int integers[], int length)
+int sum_if_a_palinedrome(int integers[], int length)
 {
+if (length < 0){
+return -2;
+}
 int sum;
-if (is_a_palinedrome(integers, length) == 1){
+if (is_a_palinedrome(integers, length) == true){
 sum = sum_elements(integers, length);
-return sum;
 }else{
 sum = -2;
 }
