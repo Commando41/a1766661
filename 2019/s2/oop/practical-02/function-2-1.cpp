b819530737119e12deb6a52041cfb953ee01ca08
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-09 12:58:32 +0000

    Update -2-1

--- 2019/s2/oop/practical-02/function-2-1.cpp
+++ 2019/s2/oop/practical-02/function-2-1.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <string.h>
+
 using namespace std;
 
 void print_as_binary(std::string decimal_number){
 
 int number = stoi(decimal_number);
+cout << number << endl;
 int binary[100]; 
 int i = 0;
-//int j = number;
-
 
 while (number > 0){
-    if (number % 2 == 0){
+    if (number % 2 == 1){
     number = (number - 1) / 2;
+    binary[i] = 1;
     }else{
     number = number / 2;
+    binary[i] = 0;
     }
+    cout << number << endl;
     i++;
 }
 for (int p = 0; p < i; p++){
