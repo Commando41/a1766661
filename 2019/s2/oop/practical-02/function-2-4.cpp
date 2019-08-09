9e32c30b3a7ff4dc7f29a5a6b67e1dc4cf799f40
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-09 13:36:09 +0000

    Update

--- 2019/s2/oop/practical-02/function-2-4.cpp
+++ 2019/s2/oop/practical-02/function-2-4.cpp
@@ -1,8 +1,28 @@
-extern int min_integer(int integers[], int langth);
+int min_integer(int integers[], int length){
+int min = integers[0];
+for (int i = 0; i < length; i++){
+if (min > integers[i]){
+min = integers[i];
+}
+}
+return min;
+}
 
-extern int max_integer(int integers[], int length);
+int max_integer(int integers[], int length){
+int max = integers[0];
+for (int i = 0; i < length; i++){
+if (max < integers[i]){
+max = integers[i];
+}
+}
+return max;
+}
 
 int sum_min_and_max(int integers[], int length){
+
+if (length < 1){
+return -1;
+}
 
 int sum;
 
