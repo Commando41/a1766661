787f71585272e031e582988f4d384b013c54f590
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-15 05:42:00 +0000

    IUpdate

--- 2019/s2/oop/practical-exam-01/function-1-1.cpp
+++ 2019/s2/oop/practical-exam-01/function-1-1.cpp
@@ -4,7 +4,7 @@ void printer(int array[10][10]){
 
 for (int i = 0; i < 10; i++){
 for (int j = 0; j < 10; j++){
-if (i == 9){
+if (j == 9){
 std::cout << array[i][j];
 }else{
 std::cout << array[i][j] << " ";
