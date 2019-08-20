eec26fa009971c90abe76c9e64c35c3e3fb896ae
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-20 02:20:01 +0000

    Update -1-1

--- 2019/s2/oop/practical-04/function-1-1.cpp
+++ 2019/s2/oop/practical-04/function-1-1.cpp
@@ -5,15 +5,7 @@ void copy_2d_strings(string first[][2], string second[][2], int length){
 for (int i = 0; i < length; i++){
 for (int j = 0; j < length-1; j++){
 second[i][j] = first[i][j];
-if (j == length-1){
-cout << second[i][j];
 }
-else
-{
-cout << second[i][j] << " ";
-}
-}
-cout << endl;
 }
 
 
