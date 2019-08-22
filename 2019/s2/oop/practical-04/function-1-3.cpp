ec98793720a496b364a074b5135f55d2a139c298
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-22 01:00:24 +0000

    -1-4

--- 2019/s2/oop/practical-04/function-1-3.cpp
+++ 2019/s2/oop/practical-04/function-1-3.cpp
@@ -3,11 +3,11 @@
 void cpyia(int old_array[], int new_array[], int length){
 
 int* ptr1 = new int[length];
-
-ptr1 = &old_array[0];
+int* ptr2 = new int[length];
+ptr1 = old_array;
 //cout << *ptr1 << endl;
 for(int i = 0; i < length; i++){
-new_array[i] = *(ptr1 + i);
+*(ptr2+i) = *(ptr1 + i);
 //cout << *(ptr1 + i) << endl;
 }
 return;
