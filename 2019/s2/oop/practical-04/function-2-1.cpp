ec607caebb6b4cf04b02d60d936bceb50cb0e6b6
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-23 11:32:16 +0000

    Update for 2-1

--- 2019/s2/oop/practical-04/function-2-1.cpp
+++ 2019/s2/oop/practical-04/function-2-1.cpp
@@ -3,8 +3,8 @@ using namespace std;
 void print_sevens(int *nums, int length){
 
 for (int i = 0; i < length; i++){
-if (*nums++ % 7 == 1){
-cout << *nums++ << endl; 
+if (nums % 7 == 0){
+cout << nums[i] << endl; 
 }
 }
 
