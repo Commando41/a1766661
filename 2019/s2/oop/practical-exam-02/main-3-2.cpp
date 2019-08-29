3b795f2c106f1c36f03064b29e8bbc1e5c6098f5
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-29 05:42:34 +0000

    y

--- 2019/s2/oop/practical-exam-02/main-3-2.cpp
+++ 2019/s2/oop/practical-exam-02/main-3-2.cpp
@@ -4,16 +4,16 @@
 extern int *readNumbers(int n);
 extern bool equalsArray(int *A, int *B, int length);
 extern void printNumbers(int *numbers, int length);
+extern int *negateArray(int *numbers, int length);
 
 int main(){
 
 	int *setA = readNumbers(4);
-	int *setB = readNumbers(4);
+	int *setB = negateArray(setA,4);
 	bool equals;
 	equals = equalsArray(setA, setB, 4);
 	printNumbers(setA, 4);
 	std::cout << equals << std::endl;
 return 0;
 }
-
 
