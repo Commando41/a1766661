fafdba3fd79441b7f365f2c5759819d59a776d94
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-08-29 05:19:25 +0000

    TRY

---
+++ 2019/s2/oop/practical-exam-02-PRO5/function-2-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int *readNumbers(int n){
+
+	int array = new int[n];
+	for(int i = 0; i < n; i++){
+	cin >> *(array+i);	
+	}
+
+}
+
+void grades(int *gradespoint, int length){
+
+	for(int i = 0; i < length; i++){
+		switch (gradespoint+i){
+			case 1: cout << i << *(gradespoint+i) << "Fail" << endl;
+			break;
+			case 2: cout << i << *(gradespoint+i) << "Pass" << endl;
+			break;
+			case 3: cout << i << *(gradespoint+i) << "Credit" << endl;
+			break;
+			case 4: cout << i << *(gradespoint+i) << "Distinction" << endl;
+			break;
+			case 5: cout << i << *(gradespoint+i) << "Higher distinction" << endl;		
+			break;
+		}	
+	}
+
+return ;
+}
