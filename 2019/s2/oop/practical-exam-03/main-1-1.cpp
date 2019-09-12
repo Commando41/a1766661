615e89807d462fb238af2d9af66c72190300e8c4
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-09-12 05:04:04 +0000

    Up

---
+++ 2019/s2/oop/practical-exam-03/main-1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Music_box.h"
+using namespace std;
+int main(){
+
+	Music_box song1("Jerold", 4);
+	
+	song1.get_song();
+	song1.get_width();
+
+return 0;
+}
