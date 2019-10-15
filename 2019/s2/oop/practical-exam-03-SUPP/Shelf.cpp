7d78b7d37d64ab138536fd3d6f4761c275df1587
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-10-15 00:57:42 +0000

    Updte

--- 2019/s2/oop/practical-exam-03-SUPP/Shelf.cpp
+++ 2019/s2/oop/practical-exam-03-SUPP/Shelf.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 Shelf::Shelf()
 {
-	cout <<"New shelf" << endl;
+	//cout <<"New shelf" << endl;
 	Awidth = 0;
 	currentWidth = 0;
 	//count = 0;
@@ -14,20 +14,20 @@ Shelf::Shelf()
 Shelf::Shelf(int width)
 {
 	//count = 0;	
-	cout <<"New shelf" << endl;
+	//cout <<"New shelf" << endl;
 	Awidth = width;
 	currentWidth = width;
 }
 
 int Shelf::get_width()
 {
-	cout << "Returned width" << endl;	
+	//cout << "Returned width" << endl;	
 	return Awidth;
 }
 
 int Shelf::get_number_of_music_boxes()
 {
-	cout << "Returned count" << endl;
+	//cout << "Returned count" << endl;
 	return count;
 }
 
@@ -35,10 +35,10 @@ Music_box *Shelf::get_contents()
 {
 	for(int i = 0; i < count; i++)
 	{
-		cout << "Returned Box[" << i << "]"<< endl;
+		//cout << "Returned Box[" << i << "]"<< endl;
 		Box[i];
 	}
-	cout << "Returned Box" << endl;
+	//cout << "Returned Box" << endl;
 	return Box;
 }
 
@@ -49,10 +49,10 @@ bool Shelf::add_music_box(Music_box a_music_box)
 		Box[count] = a_music_box;
 		currentWidth = currentWidth - a_music_box.width;
 		count++;
-		cout << "Returned true" << endl;
+		//cout << "Returned true" << endl;
 		return true;
 	}else{
-		cout << "Returned false" << endl;
+		//cout << "Returned false" << endl;
 		return false;
 	}
 }
