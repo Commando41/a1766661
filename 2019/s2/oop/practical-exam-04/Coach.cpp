4e690f0800fce9e928cef83c968256be6a9f23ec
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-10-31 05:12:49 +0000

    Sort

--- 2019/s2/oop/practical-exam-04/Coach.cpp
+++ 2019/s2/oop/practical-exam-04/Coach.cpp
@@ -7,11 +7,22 @@ using namespace std;
 
 int Coach::nextID = 0;
 
+Coach::Coach()
+{
+	salary = 0;
+	personID = nextID;
+	nextID++;
+	serviceLength = 0;
+	name = "";
+	occupation = "";
+}
+
 Coach::Coach(string n, int sl):Person(n,"Coach", sl)
 {
 	personID = nextID;
 	nextID++;
 	serviceLength = sl;
+	salary = 0;
 }
 
 int Coach::get_salary()
