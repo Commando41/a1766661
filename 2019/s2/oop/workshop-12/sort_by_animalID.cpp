d021e972481e56fbf1d24635546be346082e323e
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-10-31 00:56:00 +0000

    vegie

--- 2019/s2/oop/workshop-12/sort_by_animalID.cpp
+++ 2019/s2/oop/workshop-12/sort_by_animalID.cpp
@@ -7,18 +7,27 @@ void sort_by_animalID::sort(animal **animals, int n)
 {
 	animal *temp;
 	bool ascending = false;
+	bool swapped = false;
+
 	while(ascending == false)
 	{
+
+		swapped = false;
+
 		for(int i = 0; i < n-1; i++)
 		{
 			ascending = true;
-			if(animals[i]->get_animalID() > animals[i+1]->get_animalID())
-			{
-				temp = animals[i]; 
-				animals[i] = animals[i+1];
-				animals[i+1] = temp;
-				ascending = false;
+
+			if (swapped == false) {
+				if(animals[i]->get_animalID() > animals[i+1]->get_animalID())
+				{
+					temp = animals[i]; 
+					animals[i] = animals[i+1];
+					animals[i+1] = temp;
+					ascending = false;
+					swapped = true;
+				}
 			}
-		}
+		}	
 	}
 }
