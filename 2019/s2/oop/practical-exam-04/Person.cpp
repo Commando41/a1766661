c3c6617b9e8c9542d29bf46ec2128963e4cb4a86
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-10-31 03:57:31 +0000

    K

---
+++ 2019/s2/oop/practical-exam-04/Person.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "person.h"
+
+using namespace std;
+
+Person::Person(string n, string o, int sl)
+{
+	name = n;
+	occupation = o;
+	salary = sl;
+}
+
+void Person::set_name(string n)
+{
+	name = n;
+}
+
+void Person::set_occupation(string o)
+{
+	occupation = o;
+}
+
+void Person::set_salary(int sl)
+{
+	salary = sl;
+}
+
+string Person::get_name()
+{
+	return name;
+}
+
+string Person::get_occupation()
+{
+	return occupation;
+}
+
+
