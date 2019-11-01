aca55c831087269f7c14e56c3b3691d93140531e
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2019-11-01 03:25:29 +0000

    Good day

---
+++ 2019/s2/oop/practical-exam-04/SortPerson.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Person.h"
+#include "Player.h"
+#include "Coach.h"
+#include "SortPerson.h"
+#include <string>
+
+using namespace std;
+
+class SortPerson
+{
+public:
+void sort(Person ** people, int n)
+{
+	
+}
