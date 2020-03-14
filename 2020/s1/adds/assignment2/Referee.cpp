577d5c41c2a3724f23495873000444eac5a54a5b
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-03-14 06:50:21 +0000

    fixed the files3

--- 2020/s1/adds/assignment2/Referee.cpp
+++ 2020/s1/adds/assignment2/Referee.cpp
@@ -23,12 +23,12 @@ void Referee::matchCalc(Human *player, Computer *AI)
 	{
 		if(Pchoice[i] == 'S' && Cchoice == 1 || Pchoice[i] == 'R' && Cchoice == 2 || Pchoice[i] == 'P' && Cchoice == 3)
 		{
-			matches = matches + "L";
+			matches = matches + "L ";
 		}else if (Pchoice[i] == 'R' && Cchoice == 1 || Pchoice[i] == 'P' && Cchoice == 2 || Pchoice[i] == 'S' && Cchoice == 3)
 		{
-			matches = matches + "T";
+			matches = matches + "T ";
 		}else{
-			matches = matches + "W";
+			matches = matches + "W ";
 		}
 	}
 }
