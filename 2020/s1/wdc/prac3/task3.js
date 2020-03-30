e5b33c85ac65a47872847fb6577c8659a80be508
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-03-30 04:12:23 +0000

    fin task3-3 again

--- 2020/s1/wdc/prac3/task3.js
+++ 2020/s1/wdc/prac3/task3.js
@@ -22,16 +22,15 @@ function postStuff()
 
 }
 
-function hide()
+function hideMain()
 {
-    var m = document.getElementById("main");
-    var l = document.getElementById("menu");
-    if(m.style.display === "block")
-    {
-        m.style.display = "none";
-        l.style.display = "block";
-    }else{
-        m.style.display = "block";
-        l.style.display = "none";
-    }
+    document.getElementById("main").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+
+}
+
+function hideMenu()
+{
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("main").style.display = "block";
 }
