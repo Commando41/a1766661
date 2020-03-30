d974fc51c54ed6110ec628786eb147ef866710c5
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-03-30 02:36:33 +0000

    fin task3-2

--- 2020/s1/wdc/prac3/task3.js
+++ 2020/s1/wdc/prac3/task3.js
@@ -1,7 +1,23 @@
 var p = 0;
+
 function addOne()
 {
     p += 1;
     document.getElementById("mcount").innerText = p;
+
+}
+
+function postStuff()
+{
+
+    var post_con = document.createElement("p");
+    post_con.className = "post-content";
+    var post_t = document.createElement("pre");
+    post_t.className = "post-time";
+    post_t.innerHTML = new Date();
+    post_con.innerHTML = document.getElementsByClassName("postText")[0].value;
+    var post_text = document.getElementById("posts");
+    post_text.appendChild(post_t);
+    post_text.appendChild(post_con);
 
 }
