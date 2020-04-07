d4651a5820eb498c9191168d91ea2aefbae8b934
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-04-07 11:28:37 +0000

    task3.2

--- 2020/s1/wdc/prac4/routes/index.js
+++ 2020/s1/wdc/prac4/routes/index.js
@@ -11,5 +11,35 @@ router.get('/last.txt', function(req, res, next) {
   res.send(timestamp+"");
   timestamp = new Date();
 });
+var Cindex = 1;
+var text_color = "";
+router.get('/color.html', function(req, res, next) {
+    if(Cindex == 1){
+        text_color = "red";
+    }else if (Cindex == 2){
+        text_color = "yellow";
+    }else if(Cindex == 3){
+        text_color = "green";
+    }else if(Cindex == 4){
+        text_color = "blue";
+    }
+    if(Cindex == 4){
+        Cindex = 1;
+    }else{
+        Cindex++;
+    }
+  res.send('\
+<!DOCTYPE html>\
+<html lang="en-US">\
+<head>\
+  <meta charset="UTF-8">\
+ <title>Express</title>\
+</head>\
+<body>\
+  <h1 style="color:' + text_color + '">' + text_color + '</h1>\
+</body>\
+</html>\
+');
+});
 
 module.exports = router;
