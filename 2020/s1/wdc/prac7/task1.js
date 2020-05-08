955a621a1b6cf42eaadf70008cccd40ee79ec89a
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-05-08 12:18:36 +0000

    task1.8

--- 2020/s1/wdc/prac7/task1.js
+++ 2020/s1/wdc/prac7/task1.js
@@ -69,5 +69,9 @@ var vueinst = new Vue({
         carr: [],
         topmenuitem: 0,
         topmenuhover: false,
+        sarr: [{ name:'Adelaide City',  address:'45 Florabunda Lane, Adelaide, 5000', counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg/320px-11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg' },
+               { name:'Steelton South', address:'77 Weigall Avenue, Steelton, 5413',  counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/4/42/Well-shop-front.jpg' },
+               { name:'Milton',         address:'33 McGregor Street, Milton, 5880',   counter: 0, img:'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Greggs_store_front.JPG/320px-Greggs_store_front.JPG' }
+               ],
     }
 });
