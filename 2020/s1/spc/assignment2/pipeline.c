254c631eada403169d631681f7e5e2b09d1d07c0
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-05-17 11:39:45 +0000

    try part b 1

--- 2020/s1/spc/assignment2/pipeline.c
+++ 2020/s1/spc/assignment2/pipeline.c
@@ -9,10 +9,10 @@ int main()
 {
 	//Setup a character array to store input data
 	char inputtxt[100];
-
+	int newPipe[2], prevPipe[2];
 	//Saves the texts into an array
 	char *stuff[10];
-
+	int id;
 	//Store the value for a fork function
 	int viden;
 
@@ -24,9 +24,53 @@ int main()
 	{
 		if(!feof(fp))
 		{
-			pipe(fp);
+			if(pipe(newPipe) == -1)
+			{
+				printf("Failed making pipe \n");
+			}
+		}
+		stuff[0] = strtok(inputtxt, " \n");
+
+		int i = 0;
+
+		while(stuff[i])
+		{
+			i++;
+			stuff[i] = strtok(NULL, " \n");
+		}
+
+		if((viden = fork()) < 0)
+		{
+			printf("Did not work!\n");
+			return 1;
+		}
+		else 
+		{
+			if(id > 0)
+			{
+				//Parent
+				wait(NULL);
+
+				prevPipe[0] = newPipe[0];
+				prevPipe[1] = newPipe[1];
+				close(newPipe[0]);
+				close(newPipe[1]);
+			}
+			else
+			{
+				if(id == 0)
+				{
+					if(execvp(stuff[0],stuff) == -1)
+					{
+						//Display fail
+						printf("Failed.\n");
+					}
+				}
+			}
 		}
 	}	
 
+
+	fclose(fp);
 	return 0;
 }
