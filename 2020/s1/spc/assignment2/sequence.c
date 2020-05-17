d9c6425714e57ed4873310bc6beb6a2f2e917a1d
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-05-17 04:13:16 +0000

    trask1

--- 2020/s1/spc/assignment2/sequence.c
+++ 2020/s1/spc/assignment2/sequence.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 //Create a file pointer object
 FILE *fp;
 int main()
 {
 	//Setup a character array to store input data
 	char inputtxt[100];
-	//int max = 1;
-	char *stuff;
-	//int i = 0;
+
+	char *stuff[10];
+
+	int viden;
+
 	//Acquire the location of the file
 	fp= fopen("/dev/stdin", "r");
-	stuff = strtok(inputtxt,"\n");
-	//Receives as input all the lines within the file
-	while(fgets(inputtxt, 100, fp) > 0){
-		//Displays each line taken from file
-		stuff = strtok(inputtxt,"\n");
 
-		//i++;
-		//if(i == max){
-		//	max++;
-	//		suff = (char*)realloc(stuff,max);
-	//	}
-		//execvp(stuff,stuff);
-		//puts(inputtxt);
-		printf("%s\n",stuff);
+	//Receives as input all the lines within the file
+	while(fgets(inputtxt, 100, fp) > 0)
+	{
+		stuff[0] = strtok(inputtxt, " \n");
+		int i = 0;
+		while(stuff[i] != NULL)
+		{
+			i++;
+			stuff[i] = strtok(NULL, " \n");
+		}
+
+		viden = fork();
+		if(viden > 0){
+		wait(NULL);
+		}
+		else
+		{
+			if(viden < 0)
+			{
+				printf("Error\n");
+				return 1;
+			}
+			else
+			{
+				if(viden == 0)
+				{
+					if(execvp(stuff[0],stuff) == -1)
+					{
+					printf("Failed.\n");
+					}
+				}
+			}
+		}
 	}
 
-	//free(stuff);
 	//Close the file when done
 	fclose(fp);
 	return 0;
