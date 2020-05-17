53b6547b14e74828dde8fa0a002a1a0ab82a0fe7
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-05-17 04:59:29 +0000

    try again

--- 2020/s1/spc/assignment2/sequence.c
+++ 2020/s1/spc/assignment2/sequence.c
@@ -9,8 +9,10 @@ int main()
 	//Setup a character array to store input data
 	char inputtxt[100];
 
+	//Saves the texts into an array
 	char *stuff[10];
 
+	//Store the value for a fork function
 	int viden;
 
 	//Acquire the location of the file
@@ -19,20 +21,29 @@ int main()
 	//Receives as input all the lines within the file
 	while(fgets(inputtxt, 100, fp) > 0)
 	{
-		stuff[0] = strtok(inputtxt, " \n");
+		//Make a index to use for the array
 		int i = 0;
-		while(stuff[i] != NULL)
+
+		//Saves the first text as the first element so the nextsubstrings can be access by passing a pointer
+		stuff[0] = strtok(inputtxt, " \n");
+
+		//Whenever a NULL is called, uses the inputtxt value to return the next substring.
+		while(stuff[i])
 		{
 			i++;
 			stuff[i] = strtok(NULL, " \n");
 		}
 
+		//Make a child process 
 		viden = fork();
+
+		//When the fork is positive it will wait
 		if(viden > 0){
-		wait(NULL);
+			wait(NULL);
 		}
 		else
 		{
+			//If the fork is nefgative it will display error to user
 			if(viden < 0)
 			{
 				printf("Error\n");
@@ -40,11 +51,13 @@ int main()
 			}
 			else
 			{
+				//Otherwise if it is 0 but the execvp funtion doesnt work
 				if(viden == 0)
 				{
 					if(execvp(stuff[0],stuff) == -1)
 					{
-					printf("Failed.\n");
+						//Display fail
+						printf("Failed.\n");
 					}
 				}
 			}
