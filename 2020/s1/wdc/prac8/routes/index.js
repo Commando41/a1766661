844a3afa28f3c2ea5441e08fa4b301ae597dc3b7
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-05-19 11:13:01 +0000

    task1.2

--- 2020/s1/wdc/prac8/routes/index.js
+++ 2020/s1/wdc/prac8/routes/index.js
@@ -6,6 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mess = 'first';
+router.post('/pass-it-on', function(req, res, next){
+   if((req.body.constructor === Object && Object.keys(req.body).length === 0) || !('message' in req.body)){
+       res.send(400);
+   }else
+   {
+       res.send(mess);
+       mess = req.body.message;
+   }
+});
+
 router.get('/brew', function(req, res, next) {
    if(req.query.drink == 'tea')
    {
