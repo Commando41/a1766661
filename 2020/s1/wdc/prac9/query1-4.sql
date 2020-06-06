e4c0e404e9b75577124abeee14b9dccd075ddaf1
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-06-06 11:56:15 +0000

    1-4 again

--- 2020/s1/wdc/prac9/query1-4.sql
+++ 2020/s1/wdc/prac9/query1-4.sql
@@ -1,4 +1,4 @@
-SELECT address.address FROM address INNER JOIN store ON store.address_id = address.address_id
+SELECT DISTINCT address.address FROM address INNER JOIN store ON store.address_id = address.address_id
 INNER JOIN inventory ON inventory.store_id = store.store_id
 INNER JOIN film ON film.film_id = inventory.film_id
 WHERE film.title = "TWISTED PIRATES";
