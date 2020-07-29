3d573181e395007f7a842ceb512c3ea0ff28ba6d
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-07-29 03:08:38 +0000

    addding pssd

---
+++ 2020/s2/pssd/week01practice/Inchworm.hpp
@@ -0,0 +1,13 @@
+
+using namespace std;
+ 
+ 
+class Inchworm
+{
+  public:
+  int lunchtime(int branch, int rest, int leaf)
+  {
+    // your code for the problem
+    return res;  // return your result
+  }
+};
