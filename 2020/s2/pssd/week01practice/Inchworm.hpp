f31ce5773299f0814a00ae107e2269b142c01812
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-07-31 05:15:07 +0000

    do

--- 2020/s2/pssd/week01practice/Inchworm.hpp
+++ 2020/s2/pssd/week01practice/Inchworm.hpp
@@ -8,6 +8,13 @@ class Inchworm
   int lunchtime(int branch, int rest, int leaf)
   {
     // your code for the problem
+    int i, res = 0;//, leafy = leaf; 
+    for(i = 0; i <= branch; i+=rest)
+    {
+        if(i % leaf == 0){
+            res++;
+        }
+    }
     return res;  // return your result
   }
 };
--- 2020/s2/pssd/week01practice/main.cpp
+++ 2020/s2/pssd/week01practice/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "Inchworm.hpp"
+using namespace std;
 
 int main(){
 
-
-
+	Inchworm css;
+	int branch, rest, leaf;
+	cout << "Enter value for branch" << endl;
+	cin >> branch;
+	cout << "Enter value for rest" << endl;
+	cin >> rest;
+	cout << "Enter value for leaf" << endl;
+	cin >> leaf;
+	cout << "Amount of leaves eaten is: " << css.lunchtime(branch, rest, leaf) << endl;
     return 0;
 }
