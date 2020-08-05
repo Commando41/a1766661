56a5ac794ef8767d5c2d2e0938c1ee6611c4c35e
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-08-05 04:15:22 +0000

    diaga2

---
+++ 2020/s2/pssd/diag-a2/BootsExchange.hpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include <vector>
+
+class BootsExchange {
+	public:
+		leastAmount(vector<int> left, vector<int> right){
+			int len = right.size();
+			int check[len] = 0;
+			int d, i, miss = 0;
+			
+			for(d = 0; d < len; d++){
+			for(i = 0; i < len; i++){
+				if(left[d] == right[i]){
+					if(check[i] == 0){
+						check[i] = 1;
+						d = i = 0;
+						miss++;
+					}
+						
+				}
+
+			}
+			}			
+	}
+};
