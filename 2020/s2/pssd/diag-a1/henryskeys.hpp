5366e05daa06851dff26c51ee285f51822ce9bc7
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-08-05 04:17:08 +0000

    diaga1

---
+++ 2020/s2/pssd/diag-a1/henryskeys.hpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include <vector>
+#include <string>
+
+class Henryskey {
+
+	public:
+		int howMany(string S){
+		vector<string, att>;
+		int len = strlen(s);
+		for(int i = 0; i < len; i++;){
+			att.push_back(s);		
+		}
+		int i, d;
+		int atlen = att.length();
+		for( i = 0; i<len; i++){
+			for(d = 0; d < len; d++){
+							
+			}			
+		}
+	}	
+}
