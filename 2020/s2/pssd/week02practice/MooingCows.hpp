18bdd583b0600861dc0ed3b5beac5e584c0700c8
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-08-14 11:30:18 +0000

    Mooing

---
+++ 2020/s2/pssd/week02practice/MooingCows.hpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <vector>
+#include <string>
+
+class MooingCows{
+	public:
+		int dissatisfaction(vector<string> farmland){
+			int lenr = farmland.size();
+			int lenc = farmland[0].size();
+			int dist = 10^5;
+			int cur;
+			for(int x = 0; x < lenr; x++){
+				for(int y = 0; y < lenc; y++){
+					if(farmland[x][y] == 'C'){
+						cur = 0;
+						for(int i = 0; i < lenr; i++){
+							for(int j = 0; j < lenc; j++){
+								if(farmland[i][j] == 'C')
+								cur += (x-i)*(x-i) + (y-j)*(y-j);
+							}
+						}
+						if(cur < dist)
+							dist = cur;
+					}
+				}
+			}
+			return dist;
+
+		}	
+};
