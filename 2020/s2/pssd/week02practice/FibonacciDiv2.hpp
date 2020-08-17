287ab7b7ff4b4ab7c0d0496bb09ff78622f4fa9f
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-08-17 10:42:21 +0000

    FibonacciDiv2

---
+++ 2020/s2/pssd/week02practice/FibonacciDiv2.hpp
@@ -0,0 +1,26 @@
+using namespace std;
+#include <algorithm>
+
+class FibonacciDiv2{
+public:
+	int find(int N){
+		int fib[31];
+		fib[0] = 0;
+		fib[1] = 1;
+		fib[15] = 610;
+		fib[16] = 987;
+		fib[30] = 832040;
+		int i;
+		for(i = 2; i < 15; i++){
+			fib[i] = fib[i-2] + fib[i-1];
+			fib[i+15] = fib[i+13] + fib[i+14];
+		}
+
+		for(i = 0; i < 30; i++){
+			if(N >= fib[i] && N < fib[i+1])
+				return min(N-fib[i],fib[i+1]-N);
+		}
+
+		return N - fib[30];
+	}
+};
