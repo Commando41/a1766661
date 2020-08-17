8b5e6bda2a6cceea3c0ae3d03dab0890747d8738
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-08-17 12:54:52 +0000

    DerSeq

--- 2020/s2/pssd/week02practice/DerivativeSequence.hpp
+++ 2020/s2/pssd/week02practice/DerivativeSequence.hpp
@@ -7,19 +7,14 @@ class DerivativeSequence{
 			//vector< vector<int> > deri;
 			//deri.push_back(a);
 
-			int i, check, len = a.size();
+			int i, len = a.size();
 			while(n > 0){
-				check = 0;
 				for(i = 0; i < len-1; i++){
 					a[i] = a[i+1] - a[i];
-					if(a[i] == 0)
-						check++;
 				}
 				a.pop_back();
 				n--;
 				len--;
-				if(check == len)
-					return a;
 			} 
 			return a;
 		}
