d1e0d4912139b3102491277b04a0c85e5d08c9ce
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-08-31 08:36:08 +0000

    Monsters 3

--- 2020/s2/pssd/week04practice/MonstersValley2.hpp
+++ 2020/s2/pssd/week04practice/MonstersValley2.hpp
@@ -1,22 +1,24 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 class MonstersValley2{
 	public:
 		int minimumPrice(vector<int> dread, vector<int> price){
-			for(int i = 0; i < dread.size(); i++)
-				cout << dread[i] << " ";
-			cout << endl;
-			for(int i = 0; i < dread.size(); i++)
-				cout << price[i] << " ";
-			cout << endl;
-			int power = dread[0], cost = price[0];
+			// for(int i = 0; i < dread.size(); i++)
+			// 	cout << dread[i] << " ";
+			// cout << endl;
+			// for(int i = 0; i < dread.size(); i++)
+			// 	cout << price[i] << " ";
+			// cout << endl;
+			long long power = 0;
+			int cost = price[0];
 			price.erase(price.begin());
 			dread.erase(dread.begin());
 			for(int i = 0; i < dread.size(); i++){
 				if(power < dread[i]){
 					if(price[i] == 1){
-						power += dread[i];
+						power += (long long)dread[i];
 						cost += price[i];
 						dread.erase(dread.begin() + i);
 						price.erase(price.begin() + i);
@@ -24,8 +26,8 @@ class MonstersValley2{
 					}else{
 						int ps = 0;
 						for(int d = 0; d < i; d++){
-							if(power + dread[d] >= dread[i] && price[d] == 1){
-								power += dread[d];
+							if(power + dread[d] > dread[i] && price[d] == 1){
+								power += (long long)dread[d];
 								cost += price[d];
 								dread.erase(dread.begin() + d);
 								price.erase(price.begin() + d);
@@ -35,7 +37,7 @@ class MonstersValley2{
 							}
 						}
 						if(ps == 0){
-							power += dread[i];
+							power += (long long)dread[i];
 							cost += price[i];
 							dread.erase(dread.begin() + i);
 							price.erase(price.begin() + i);
