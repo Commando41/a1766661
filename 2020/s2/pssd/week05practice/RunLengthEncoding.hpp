d295fc8c83b126e3c2fd1e409eff36317cb91e8e
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-05 04:28:55 +0000

    RunLengthEncoding

---
+++ 2020/s2/pssd/week05practice/RunLengthEncoding.hpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <cctype>
+using namespace std;
+
+class RunLengthEncoding{
+	public:
+		string decode(string text){
+			string display, number = "1";
+			int len = text.size();
+			int total = 0;
+			for(int i = 0; i < len; i++){
+				if(isdigit(text[i])){
+					number += text[i];
+				}else{
+					int num;
+					if(number == " ")
+						num = 1;
+					else
+						num = stoi(number);
+					total += num;
+					if(total > 50)
+						return "TOO LONG";
+					number = " ";
+					for(int m = 0; m < num; m++)
+						display = display + text[i];
+				}
+			}
+			return display;	
+		}
+};
