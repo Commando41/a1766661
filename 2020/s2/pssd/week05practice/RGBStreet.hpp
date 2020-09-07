78e9615bdfc3ced92ae3ae8b3a40c61de0d8c476
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-07 08:45:07 +0000

    Bonus

--- 2020/s2/pssd/week05practice/RGBStreet.hpp
+++ 2020/s2/pssd/week05practice/RGBStreet.hpp
@@ -7,10 +7,10 @@ using namespace std;
 
 class RGBStreet{
 	public:
-		vector<int> make(string house){
+/*		vector<int> make(string house){
 			stringstream ss;
 			ss << house;
-			vector<int> len{0, 0, 0};
+			vector<int> len;
 			string numbs = "HA";
 			numbs.clear();
 			for(int i = 0; i < 3; i++){
@@ -19,24 +19,32 @@ class RGBStreet{
 				numbs.clear();
 			}	
 			return len;		
-		}
-		int recur(vector<string> houses, int choose, int index, int amount){
-			if(houses.size() <= index)
+		}*/
+		int recur(vector<int> len, int choose, int index, int amount){
+			if(len.size()/3 <= index)
 				return amount;
-
-			vector<int> len = make(houses[index]);
 			
 			if(choose == 0)
-				return min(recur(houses, 1, index + 1, amount + len[1]), recur(houses, 2, index + 1, amount + len[2]));
+				return min(recur(len, 1, index + 1, amount + len[index*3 + 1]), recur(len, 2, index + 1, amount + len[index*3 + 2]));
 			else if(choose == 1)
-				return min(recur(houses, 0, index + 1, amount + len[0]), recur(houses, 2, index + 1, amount + len[2]));
+				return min(recur(len, 0, index + 1, amount + len[index*3 + 0]), recur(len, 2, index + 1, amount + len[index*3 + 2]));
 			else
-				return min(recur(houses, 0, index + 1, amount + len[0]), recur(houses, 1, index + 1, amount + len[1]));
+				return min(recur(len, 0, index + 1, amount + len[index*3 + 0]), recur(len, 1, index + 1, amount + len[index*3 + 1]));
 		}
 		int estimateCost(vector<string> houses){
 			int index = 1;
-			vector<int> len = make(houses[0]);
+			vector<int> len;
+			for(int i = 0; i < houses.size(); i++){
+				stringstream ss;
+				string numbs1, numbs2, numbs3;
+				ss << houses[i];
+				ss >> numbs1 >> numbs2 >> numbs3;
+				int nun1 = stoi(numbs1), nun2 = stoi(numbs2), nun3 = stoi(numbs3);
+				len.push_back(nun1);
+				len.push_back(nun2);
+				len.push_back(nun3);
+			}
 
-			return min(recur(houses, 0, index, len[0]), min(recur(houses, 1, index, len[1]), recur(houses, 2, index, len[2])));
+			return min(recur(len, 0, index, len[0]), min(recur(len, 1, index, len[1]), recur(len, 2, index, len[2])));
 		}	
 };
