a28ec9880d402a58b05d8a8bfe499f642d843608
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-10 12:37:30 +0000

    Barbecue

--- 2020/s2/pssd/week06practice/Barbecue.hpp
+++ 2020/s2/pssd/week06practice/Barbecue.hpp
@@ -8,63 +8,63 @@ class Barbecue{
 				return 0;
 			vector<int> vo(n, 0);
 			vector<int> ex(n, 0);
-			vector<int> ppl;
-			for(int i = 0; i < n; i++)
-				ppl.push_back(i);
 			for(int i = 0; i < voter.size(); i++){
-				vo[voter[i]]++;
-				ex[excluded[i]]++;
+				int boom = voter[i];
+				vo[boom] += 1;
+				boom = excluded[i];
+				ex[boom] += 1;
 			}
-			//vector<int> even;
+			vector<int> even;
 			int high = 0;
-			for(int i = 0; i < ex.size(); i++){
-				if(high > ex[i]){
-					ex.erase(ex.begin() + i);
-					vo.erase(vo.begin() + i);
-					ppl.erase(ppl.begin() + i);
-					i--;
-				}else if(high < ex[i]){
+			for(int i = 0; i < n; i++){
+				if(high = ex[i])
+					even.push_back(i);
+				else if(high < ex[i]){
 					high = ex[i];
-					ex.erase(ex.begin(), ex.begin() + i - 1);
-					vo.erase(vo.begin(), vo.begin() + i - 1);
-					ppl.erase(ppl.begin(), ppl.begin() + i - 1);
-					i = 0;	
+					even.clear();
+					even.push_back(i);
 				}
 			}
-			if(ppl.size() == 1){
-				cout << "Chosen the most: " << ppl[0] << endl;
-				return ppl[0];
+			if(even.size() == 1){
+				cout << " Even[0] " << even[0] << endl;
+				return even[0];
 			}
-			for(int i = 0; i < ppl.size(); i++){
-				cout << ppl[i] << " ";
+			for(int i = 0; i < even.size(); i++){
+				cout << even[i] << " ";
 			}
 			cout << endl;
 			high = 0;
-			for(int i = 0; i < vo.size(); i++){
+			vector<int> boomer;
+			cout << "Boomers" << endl;
+			for(int i = 0; i < even.size(); i++){
 				//int boom = even[i];
-				if(high < vo[i]){
-					high = vo[i];
-					ex.erase(ex.begin(), ex.begin() + i - 1);
-					vo.erase(vo.begin(), vo.begin() + i - 1);
-					ppl.erase(ppl.begin(), ppl.begin() + i - 1);
-					i = 0;	
-				}else if(high > vo[i]){
-					ex.erase(ex.begin() + i);
-					vo.erase(vo.begin() + i);
-					ppl.erase(ppl.begin() + i);
-					i--;
-				}
+				if(high < vo[even[i]]){
+					high = vo[even[i]];
+					cout << "High = " << vo[even[i]] << endl;
+					boomer.clear();
+					cout << "\nNew start" << endl;
+					boomer.push_back(even[i]);
+					cout << "Added even[" << i << "]: " << even[i] << " with ex[" << even[i] << "]: " << ex[even[i]] << " and vo[" << even[i] << "]: " << vo[even[i]] << endl;
+				}else if(high = vo[even[i]]){
+					boomer.push_back(even[i]);
+					cout << "Added even[" << i << "]: " << even[i] << " with ex[" << even[i] << "]: " << ex[even[i]] << " and vo[" << even[i] << "]: " << vo[even[i]] << endl;
+				}else
+					cout << "Skipped even[" << i << "]: " << even[i] << " with ex[" << even[i] << "]: " << ex[even[i]] << " and vo[" << even[i] << "]: " << vo[even[i]] << endl;
 			}
-			if(ppl.size() == 1){
-				cout << "Voted the most: " << ppl[0] << endl;
-				return ppl[0];
+			if(boomer.size() == 1){
+				cout << "boomer[0] " << boomer[0] << endl;
+				return boomer[0];
 			}
-			for(int i = 0; i < ppl.size(); i++){
-				cout << ppl[i] << " ";
+			for(int i = 0; i < boomer.size(); i++){
+				cout << boomer[i] << " ";
 			}
 			cout << endl;
-
-			cout << "Lowest index: " << ppl[0] << endl;
-			return ppl[0];
+			int low = n;
+			for(int i = 0; i < boomer.size(); i++){
+				if(low > boomer[i])
+					low = boomer[i];
+			}
+			cout << "low " << low << endl;
+			return low;
 		}
 };
