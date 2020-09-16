09a189f5ec813be495ba46425e3e26d63832f9dc
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-16 03:42:30 +0000

    prac2a2

--- 2020/s2/pssd/pracexam2a1/Regattas.hpp
+++ 2020/s2/pssd/pracexam2a1/Regattas.hpp
@@ -4,7 +4,7 @@ using namespace std;
 class Regattas{
 	public:
 		int betResult(vector<string> regattas, string ticket){
-
+			
 			for(int i = 0; i < ticket.size()-1; i++){
 				for(int d = i+1; d < ticket.size(); d++){
 					if(ticket[i] == ticket[d])
@@ -15,15 +15,15 @@ class Regattas{
 			for(int i = 0; i < ticket.size(); i++){
 				isThere = 0;
 				for(int d = 0; d < regattas.size(); d++){
-					for(int m = 0; m < regattas[d].size(); m++){
-						if(ticket[i] == regattas[d][m]){
-							if(m == 0){
+					string temp = regattas[d];
+					for(int m = 0; m < temp.size(); m++){
+						if(ticket[i] == temp[m]){
+							if(m == 0)
 								wins++;
-								m = 0;
-								regattas.erase(regattas.begin()+d);
-								d--;
-								isThere = 1;
-							}
+							m = 0;
+							regattas.erase(regattas.begin()+d);
+							d--;
+							isThere = 1;
 						}
 					}
 				}
---
+++ 2020/s2/pssd/pracexam2a2/OnlineOrder.hpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+#include <algorithm>
+
+class OnlineOrder{
+	public:
+		int numPackets(vector<int> weights, int maxWeight){
+			int sum = 0, ret = 0;
+			sort(weights.begin(), weights.end());
+			for(int i = 0; i < weights.size(); i++){
+				sum += weights[i];
+				if(sum > maxWeight){
+					ret++;
+					sum = weights[i];
+				}
+				if(i == weights.size()-1)
+					ret++;
+			}
+			return ret;
+		}
+};
