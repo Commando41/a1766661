7eb15e86fb9973c6bf485b2c2398bffbe040d21f
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-16 03:12:58 +0000

    prac2a1

---
+++ 2020/s2/pssd/pracexam2a1/Regattas.hpp
@@ -0,0 +1,35 @@
+using namespace std;
+#include <vector>
+#include <string>
+class Regattas{
+	public:
+		int betResult(vector<string> regattas, string ticket){
+
+			for(int i = 0; i < ticket.size()-1; i++){
+				for(int d = i+1; d < ticket.size(); d++){
+					if(ticket[i] == ticket[d])
+						return -1;
+				}
+			}
+			int wins = 0, isThere;
+			for(int i = 0; i < ticket.size(); i++){
+				isThere = 0;
+				for(int d = 0; d < regattas.size(); d++){
+					for(int m = 0; m < regattas[d].size(); m++){
+						if(ticket[i] == regattas[d][m]){
+							if(m == 0){
+								wins++;
+								m = 0;
+								regattas.erase(regattas.begin()+d);
+								d--;
+								isThere = 1;
+							}
+						}
+					}
+				}
+				if(isThere == 0)
+					return -1;
+			}
+			return wins;
+		}
+};
