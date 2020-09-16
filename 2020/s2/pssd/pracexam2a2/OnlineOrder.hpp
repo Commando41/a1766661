77e43d68a70124af1f033329cd087113b3bf1100
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-16 03:45:13 +0000

    again

--- 2020/s2/pssd/pracexam2a2/OnlineOrder.hpp
+++ 2020/s2/pssd/pracexam2a2/OnlineOrder.hpp
@@ -6,7 +6,7 @@ class OnlineOrder{
 	public:
 		int numPackets(vector<int> weights, int maxWeight){
 			int sum = 0, ret = 0;
-			sort(weights.begin(), weights.end());
+			//sort(weights.begin(), weights.end());
 			for(int i = 0; i < weights.size(); i++){
 				sum += weights[i];
 				if(sum > maxWeight){
