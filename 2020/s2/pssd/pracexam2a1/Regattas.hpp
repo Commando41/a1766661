9dee304c98aba66fc51d5b7f899c873d28f299ff
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-16 04:13:33 +0000

    finally

--- 2020/s2/pssd/pracexam2a1/Regattas.hpp
+++ 2020/s2/pssd/pracexam2a1/Regattas.hpp
@@ -1,34 +1,48 @@
 using namespace std;
 #include <vector>
 #include <string>
+//#include <iostream>
 class Regattas{
 	public:
 		int betResult(vector<string> regattas, string ticket){
-			
-			for(int i = 0; i < ticket.size()-1; i++){
-				for(int d = i+1; d < ticket.size(); d++){
+			int len = ticket.size();
+			if(len > regattas.size())
+				return -1;
+			//cout << "ticket is less than or equal to regattas elements" << endl;
+			for(int i = 0; i < len-1; i++){
+				for(int d = i+1; d < len; d++){
 					if(ticket[i] == ticket[d])
 						return -1;
 				}
 			}
-			int wins = 0, isThere;
+			//cout << "no letters are the same" << endl;
+			int wins = 0, isThere = 0;
+			//int lenb = regattas.size();
+			//cout << "vars defined" << endl;
 			for(int i = 0; i < ticket.size(); i++){
 				isThere = 0;
+				//cout << "ticket char No. " << i+1 << endl;
 				for(int d = 0; d < regattas.size(); d++){
-					string temp = regattas[d];
-					for(int m = 0; m < temp.size(); m++){
-						if(ticket[i] == temp[m]){
-							if(m == 0)
+					//cout << "string No. " << d+1 << endl;
+					//string temp = regattas[d];
+					for(int m = 0; m < regattas[d].size(); m++){
+						//cout << "char of string No. " << d+1  << " is " << m+1 << endl;
+						if(ticket[i] == regattas[d][m]){
+							if(m == 0){
 								wins++;
-							m = 0;
+							}
 							regattas.erase(regattas.begin()+d);
 							d--;
 							isThere = 1;
+							break;
 						}
+						if(ticket[i] != regattas[d][m] && isThere == 0)
+							return -1;
+
 					}
+					if(isThere == 1)
+						break;
 				}
-				if(isThere == 0)
-					return -1;
 			}
 			return wins;
 		}
