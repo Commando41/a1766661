06ba6fb8de336aa70f555651c714d51ae72ca0d8
a1766661 <a1766661@535180cf-fdf8-4563-973f-9dc578d4b257>
2020-09-18 13:36:28 +0000

    week 7 LotteryTicket

--- 2020/s2/pssd/week04practice/ImportantTasks.hpp
+++ 2020/s2/pssd/week04practice/ImportantTasks.hpp
@@ -5,8 +5,23 @@ using namespace std;
 class ImportantTasks{
 	public:
 		int maximalCost(vector<int> complexity, vector<int> computers){
+
 			int lencpl = complexity.size();
 			int lencpt = computers.size();
+			if(lencpl < lencpt){
+				complexity.insert(complexity.begin(), lencpt-lencpl, -1);
+				lencpl = lencpt;
+			}else if(lencpl > lencpt){
+				computers.insert(computers.begin(), lencpl-lencpt, -1);
+				lencpt = lencpl;
+			}
+			sort(complexity.begin() ,complexity.end());
+			sort(computers.begin(), computers.end());
+
+			sort(complexity.begin() ,complexity.end());
+			sort(computers.begin(), computers.end());
+
+
 			// for(int i = 0; i < lencpl; i++)
 			// 	cout << complexity[i] << " ";
 			// cout << endl;
@@ -18,12 +33,20 @@ class ImportantTasks{
 			int d = 0;
 			int pos = 0;
 			for(int i = 0; i < lencpl; i++){
-				if(complexity[i] <= computers[d]){
+				if(complexity[i] <= computers[d] && complexity[i] != -1 && computers[d] != -1){
 					pos = pos + 1;
-				}else{
+				}else {
 					while(d < lencpt){
+
+						if(complexity[i] == -1)
+							break;
+						d++;
+
+						d++;
+
 						d = d + 1;
-						if(complexity[i] <= computers[d]){
+
+						if(complexity[i] <= computers[d] && computers[d] != -1){
 							pos = pos + 1;
 							break;
 						}
---
+++ 2020/s2/pssd/week07practice/LotteryTicket.hpp
@@ -0,0 +1,21 @@
+#include <string>
+using namespace std;
+class LotteryTicket {
+	public:
+		string buy(int price, int b1, int b2, int b3, int b4){
+
+			if(b1 == price || b2 == price || b3 == price || b4 == price
+				|| b1 + b2 == price
+				|| b1 + b3 == price
+				|| b1 + b4 == price
+				|| b2 + b3 == price
+				|| b2 + b4 == price
+				|| b3 + b4 == price
+				|| b1 + b2 + b3 == price
+				|| b1 + b2 + b4 == price
+				|| b2 + b3 + b4 == price
+				|| b1 + b2 + b3 + b4 == price)
+				return "POSSIBLE";		
+			return "IMPOSSIBLE";
+		}
+};
