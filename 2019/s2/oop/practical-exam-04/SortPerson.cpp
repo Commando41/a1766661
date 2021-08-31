8f8818a42745c9cc5cf48ae4b7e92d4f160f4421
Commando41 <50309993+Commando41@users.noreply.github.com>
2021-08-31 13:59:29 +0930

    Update SortPerson.cpp

--- 2019/s2/oop/practical-exam-04/SortPerson.cpp
+++ 2019/s2/oop/practical-exam-04/SortPerson.cpp
@@ -2,14 +2,10 @@
 #include "Person.h"
 #include "Player.h"
 #include "Coach.h"
-#include "SortPerson.h"
 #include <string>
 
 using namespace std;
 
-class SortPerson
-{
-public:
 void sort(Person ** people, int n)
 {
 	
